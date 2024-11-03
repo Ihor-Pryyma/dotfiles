5a1880d55fc3e0df2501068ac0fd768807c9702a
Ihor Pryyma <ih.pryyma@gmail.com>
2024-11-03 20:42:27 +0200

    add dap python configuration

--- .config/nvim/lua/configs/dap-python.lua
+++ .config/nvim/lua/configs/dap-python.lua
@@ -7,6 +7,46 @@ local function get_python_path()
 end
 require("dap-python").setup(path)
 
+local dap = require "dap"
+dap.adapters.python = {
+  type = "executable",
+  command = get_python_path(),
+  args = { "-m", "debugpy.adapter" },
+}
+dap.configurations.python = {
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    pythonPath = function()
+      return get_python_path()
+    end,
+    cwd = vim.fn.getcwd(),
+  },
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch @tests",
+    program = "${file}",
+    args = { "--tags", "test" },
+    pythonPath = function()
+      return get_python_path()
+    end,
+    cwd = vim.fn.getcwd(),
+  },
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch @sanity",
+    program = "${file}",
+    args = { "--tags", "sanity" },
+    pythonPath = function()
+      return get_python_path()
+    end,
+    cwd = vim.fn.getcwd(),
+  },
+}
 local map = vim.keymap.set
 
 map("n", "<leader>dpr", function()
